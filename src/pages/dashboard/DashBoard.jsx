66c31eb0ba39fdeb393bebfade04716fd513398a
Maksym Martynenko <predatar24@gmail.com>
2023-09-14 12:58:07 +0300

    dashboard: create Header

--- src/pages/dashboard/DashBoard.jsx
+++ src/pages/dashboard/DashBoard.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 
 import { Switch, Route } from 'react-router-dom';
+
+import Spinner from '../spinner/Spinner';
 
 import ROUTES from '../../routes/const';
 
@@ -15,15 +17,18 @@ const DashBoard = () => {
       <Sidebar />
       <div className="dashboard__wrapper">
         <Header />
-        <Switch>
-          <Route exact to={ROUTES.DASHBOARD_HOME}>
-            asdasdawd
-          </Route>
-          <Route exact to={ROUTES.DASHBOARD_BOARDS}>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt ipsa ea sit magni odit quisquam expedita hic
-            repudiandae odio temporibus, corrupti deserunt amet mollitia nobis assumenda! Voluptatibus quas eum dolores?
-          </Route>
-        </Switch>
+        <Suspense fallback={<Spinner />}>
+          <Switch>
+            <Route exact to={ROUTES.DASHBOARD_HOME}>
+              asdasdawd
+            </Route>
+            <Route exact to={ROUTES.DASHBOARD_BOARDS}>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt ipsa ea sit magni odit quisquam expedita
+              hic repudiandae odio temporibus, corrupti deserunt amet mollitia nobis assumenda! Voluptatibus quas eum
+              dolores?
+            </Route>
+          </Switch>
+        </Suspense>
       </div>
     </div>
   );
--- src/pages/dashboard/header/Header.jsx
+++ src/pages/dashboard/header/Header.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from 'react';
+
+import './index.scss';
+
+import img from '../../../img/personal-img.png';
 
 const Header = () => {
-	return (
-		<div>Header</div>
-	)
-}
+  return (
+    <div className="dashboard-header">
+      <div className="personal">
+        <div className="personal__img">
+          <img src={img} alt="Personal img" loading="lazy" />
+        </div>
+        <div className="personal__info">
+          <div className="personal__name">Lucy Lavender</div>
+          <div className="personal__status">
+            <div className="personal__dot"></div>
+            Online
+          </div>
+        </div>
+      </div>
+      <div className="theme">
+        <label htmlFor="day" className="radio__label">
+          <input type="radio" name="theme" id="day" value={'day'} className="radio__input" />
+          <span className="radio__custom"></span>
+          <div className="radio__text">Day theme</div>
+        </label>
+        <label htmlFor="night" className="radio__label">
+          <input type="radio" name="theme" id="night" value={'night'} className="radio__input" />
+          <span className="radio__custom"></span>
+          <div className="radio__text">Night theme</div>
+        </label>
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
