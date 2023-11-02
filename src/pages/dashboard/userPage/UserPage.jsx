c395af7b1e92002d8147972b4821c6d59f5df8ce
Maksym Martynenko <predatar24@gmail.com>
2023-11-02 13:11:40 +0200

    start User

---
+++ src/pages/dashboard/userPage/UserPage.jsx
@@ -0,0 +1,27 @@
+import React, { lazy, Suspense } from 'react';
+
+import { Switch, Route } from 'react-router-dom';
+
+import Spinner from '../../spinner//Spinner';
+
+const MainBoard = lazy(() => import('./main/MainBoard'));
+
+import ROUTES from '../../../routes/const';
+
+import './index.scss';
+
+const UserPage = () => {
+  return (
+    <div className="dashboard-board">
+      <Suspense fallback={<Spinner />}>
+        <Switch>
+          <Route exact path={ROUTES.DASHBOARD_USERS}>
+            <MainBoard />
+          </Route>
+        </Switch>
+      </Suspense>
+    </div>
+  );
+};
+
+export default UserPage;
