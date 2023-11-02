671c0daf412ddf814655b55a2eec8e6cb5cfc618
Maksym Martynenko <predatar24@gmail.com>
2023-11-02 16:41:58 +0200

    update Board main page adaptive

--- src/pages/dashboard/boardPage/main/MainBoard.jsx
+++ src/pages/dashboard/boardPage/main/MainBoard.jsx
@@ -1,8 +1,8 @@
-import React, { useCallback, useEffect } from "react";
+import React, { useCallback, useEffect } from 'react';
 
-import { Helmet } from "react-helmet";
+import { Helmet } from 'react-helmet';
 
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 import {
   MdCancel,
@@ -10,30 +10,29 @@ import {
   MdDragIndicator,
   MdLockOpen,
   MdModeComment,
-  MdMoreVert,
   MdSettings,
   MdViewHeadline,
-  MdVisibility,
-} from "react-icons/md";
+  MdVisibility
+} from 'react-icons/md';
 
-import Links from "../../../../UI/links/Links";
+import Links from '../../../../UI/links/Links';
 import {
   DashboardPopup,
   DashboardPopupItem,
-  DashboardPopupItemLink,
-} from "../../../../components/dashboardPopup/DashboardPopup";
+  DashboardPopupItemLink
+} from '../../../../components/dashboardPopup/DashboardPopup';
 
-import ROUTES from "../../../../routes/const";
+import ROUTES from '../../../../routes/const';
 
-import "./index.scss";
+import './index.scss';
 
 const MainBoard = () => {
   useEffect(() => {
-    document.querySelector(".dashboard").style = "grid-template-rows: 1fr;";
+    document.querySelector('.dashboard').style = 'grid-template-rows: 1fr;';
   }, []);
   useEffect(() => {
     return () => {
-      document.querySelector(".dashboard").style = "";
+      document.querySelector('.dashboard').style = '';
     };
   });
 
@@ -43,57 +42,41 @@ const MainBoard = () => {
         <div className="dashboard-board__drag">
           <MdDragIndicator />
         </div>
-        <div className="dashboard-board__name">
-          Send status updates back through Intercom
-        </div>
+        <div className="dashboard-board__name">Send status updates back through Intercom</div>
         <div className="dashboard-board__posts">12</div>
         <div className="dashboard-board__activities">
           <div
             className="dashboard-board__visibility"
-            onClick={(e) => {
-              e.target.classList.toggle("dashboard-board__visibility_active");
+            onClick={e => {
+              e.target.classList.toggle('dashboard-board__visibility_active');
             }}
           >
             <MdVisibility />
           </div>
           <div
             className="dashboard-board__lock"
-            onClick={(e) => {
-              e.target.classList.toggle("dashboard-board__lock_active");
+            onClick={e => {
+              e.target.classList.toggle('dashboard-board__lock_active');
             }}
           >
             <MdLockOpen />
           </div>
           <div>
-            <DashboardPopup name={"board"}>
-              <DashboardPopupItem
-                icon={<MdVisibility />}
-                text={"Public view"}
-              />
+            <DashboardPopup name={'board'}>
+              <DashboardPopupItem icon={<MdVisibility />} text={'Public view'} />
               <DashboardPopupItemLink
                 to={ROUTES.DASHBOARD_BOARDS_EDIT}
                 icon={<MdSettings />}
-                text={"Edit board settings"}
+                text={'Edit board settings'}
               />
-              <DashboardPopupItem
-                icon={<MdViewHeadline />}
-                text={"List board tags"}
-              />
+              <DashboardPopupItem icon={<MdViewHeadline />} text={'List board tags'} />
               <DashboardPopupItemLink
                 to={ROUTES.DASHBOARD_BOARDS_EDIT_BOARD}
                 icon={<MdCreate />}
-                text={"Edit board post"}
+                text={'Edit board post'}
               />
-              <DashboardPopupItemLink
-                to={ROUTES.DASHBOARD_BOARDS_DELETE}
-                icon={<MdCancel />}
-                text={"Delete board"}
-              />
-              <DashboardPopupItemLink
-                to={ROUTES.DASHBOARD_POSTS}
-                icon={<MdModeComment />}
-                text={"View posts"}
-              />
+              <DashboardPopupItemLink to={ROUTES.DASHBOARD_BOARDS_DELETE} icon={<MdCancel />} text={'Delete board'} />
+              <DashboardPopupItemLink to={ROUTES.DASHBOARD_POSTS} icon={<MdModeComment />} text={'View posts'} />
             </DashboardPopup>
           </div>
         </div>
@@ -114,22 +97,17 @@ const MainBoard = () => {
         <Helmet>
           <title>Dashboard Boards - PRONY</title>
         </Helmet>
-        <Link to={ROUTES.DASHBOARD_HOME}>Dashboard</Link>/
-        <Link to={ROUTES.DASHBOARD_BOARDS}>Boards</Link>
+        <Link to={ROUTES.DASHBOARD_HOME}>Dashboard</Link>/<Link to={ROUTES.DASHBOARD_BOARDS}>Boards</Link>
       </div>
       <div className="dashboard__main">
         <div className="dashboard__title">Boards</div>
-        <Links
-          text={"Create Board"}
-          theme={"link_primary"}
-          width={147}
-          to={ROUTES.DASHBOARD_BOARDS_CREATE}
-        />
+        <Links text={'Create Board'} theme={'link_primary'} width={147} to={ROUTES.DASHBOARD_BOARDS_CREATE} />
       </div>
       <div className="dashboard-board__table">
         <div className="dashboard-board__head">
           <div className="dashboard-board__tag">Name</div>
           <div className="dashboard-board__tag">Posts</div>
+          <div className="dashboard-board__tag"></div>
         </div>
         <div className="dashboard-board__body">{renderItems()}</div>
       </div>
